d0702800a3c048dabd9776f236c2e9e142bc6c9c
Ironbark <n.norman.va@gmail.com>
2019-02-09 14:01:16 -0500

    csv to dict and max and min obtained

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,37 @@
-# Dependencies
 import csv
 import os
 
-# Specify the file to write to
-bank_csv = os.path.join("budget_data.csv")
+bank_csv = os.path.join("./PyBank/budget_data.csv")
 
 with open(bank_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
+
     csv_header = next(csvfile)
 
     count = 0
     total = 0
-    
-    
+    valuelist = []
+    mydict = {}
+
     for row in csvreader:
         count += 1
         total = total + int(row[1])
-              
-    print (count)
-    print (total)
+        valuelist.append(int(row[1]))
+        mydict[row[0]] = int(row[1])
+
+    maxvalue = max(valuelist)
+    minvalue = min(valuelist)
+
+    for date, amount in mydict.items():
+        if amount == maxvalue:
+            maxdate = date
+        elif amount == minvalue:
+            mindate = date
+
+    print(maxdate)
+    print(mindate)
+    print(count)
+    print(total)
+    print(max(valuelist))
+    print(min(valuelist))
+    # print(valuelist)
